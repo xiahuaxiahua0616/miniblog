3f26a02f680b89a570a7cbab24c1304a883a9da9
xiahua <xhxiangshuijiao@163.com>
2025-05-15 19:03:46 +0800

    feat: 实现错误包

---
+++ internal/pkg/errno/post.go
@@ -0,0 +1,10 @@
+package errno
+
+import (
+	"net/http"
+
+	"github.com/onexstack/onexstack/pkg/errorsx"
+)
+
+// ErrPostNotFound 表示未找到指定的博客.
+var ErrPostNotFound = &errorsx.ErrorX{Code: http.StatusNotFound, Reason: "NotFound.PostNotFound", Message: "Post not found."}
